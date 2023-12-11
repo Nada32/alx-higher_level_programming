8175785440260f5a3adf2c58d03b945c84146e41
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-10 16:01:18 -0800

    D

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(c):
     for i in c:
         if i > chr(96) and i < chr(97+26):
-            print("{}".format(chr(ord(i) - 32)), end='')
-        else:
-            print("{}".format(i), end='')
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
