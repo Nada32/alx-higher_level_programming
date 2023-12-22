2fd1e2b86b15eab26fcd32131a22d7203ed418e3
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-22 17:46:52 +0000

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
 	from add_0.py import add
 	a = 1
 	b = 2
-	z = add(a,b)
-	print("{:d} + {:d} = {:d}".format(a, b, z))
+	print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
