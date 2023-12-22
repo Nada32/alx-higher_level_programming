b2f62e044231f2647a66c989a2ec8773a226b84d
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-22 16:25:23 +0000

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 	from add_0.py import add
-	z = add(1,2)
-	print("{} + {} = {}".format(1, 2, z))
+	a = 1
+	b = 2
+	z = add(a,b)
+	print("{} + {} = {}".format(a, b, z))
