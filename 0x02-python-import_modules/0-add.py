feb0137819c6943e7b00d0b3297e54a77dad168f
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-22 16:19:02 +0000

    d

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import add_0.py
-z = add_0.add(1,2)
-print("{} + {} = {}".format(1, 2, z))
+if __name__ == "__main__":
+	import add_0.py
+	z = add_0.add(1,2)
+	print("{} + {} = {}".format(1, 2, z))
