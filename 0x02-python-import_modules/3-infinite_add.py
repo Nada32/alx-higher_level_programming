082492d11950efd75115b755c059c4d13cefb639
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-30 13:58:46 -0800

    d

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,10 @@ if __name__ == "__main__":
     n = len(argv)
     z = 0
     for i in range(1, n):
-        d = int(i)
+        if i == '-':
+            d = -1 * int(i + 1)
+            i++
+        else:
+            d = int(i)
         z = z + d
     print("{:d}".format(z))
