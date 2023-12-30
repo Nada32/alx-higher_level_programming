e3099034f1f3f6db1a840cd441561b77e5477a07
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-29 22:27:55 -0800

    d

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from calculator_1.py import add
+	from calculator_1.py import add, sub, mul, div
 	a = 10
 	b = 5
 	print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
-        from calculator_1.py import sub
-        a = 10
-        b = 5
-        print("{:d} + {:d} = {:d}".format(a, b, sub(a,b)))
-        from calculator_1.py import mul
-        a = 10
-        b = 5
-        print("{:d} + {:d} = {:d}".format(a, b, mul(a,b)))
-        from calculator_1.py import div
-        a = 10
-        b = 5
-        print("{:d} + {:d} = {:d}".format(a, b, div(a,b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
