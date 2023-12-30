41908b1ffe2e91150ce41613e04d76bae3e058fa
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-30 04:29:43 -0800

    d

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     n = len(argv)
-    print("{} arguments".format(n))
+    print("{} arguments".format(n), end="")
+    if n > 0:
+        print("\n")
+    elif n == 0:
+        print(".")
     for i in range(1, n):
         if n > 0:
             print("{:d} : {}".format(i, argv[i]))
