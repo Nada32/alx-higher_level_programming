d7f566af57c899746df1c8eac4223abbe414cfce
Nada32 <nada0abou0alsoud@gmail.com>
2023-12-30 21:48:34 -0800

    d

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from  hidden_4 import * as x
+    for i in x:
+        if i != '_'
+            print("{}".format(i), end="")
+    print("\n", end="")
