d2a84fc8b60741782ec4b0487076e34f43ce31ec
Nada32 <nada0abou0alsoud@gmail.com>
2024-01-04 06:59:15 -0800

    d

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    def text_indentation(text):
+    for i in text:
+        if i == '.' or i == '?' or i== ':':
+            print(i, end="")
+            print("")
+        else:
+            print(i, end="")
+    print("")
