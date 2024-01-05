f57cf41c2f38d53df07e38a88f38178e1809f298
Nada32 <nada0abou0alsoud@gmail.com>
2024-01-05 01:08:16 -0800

    d

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if type(a) != int or type(a) != float:
+    if type(a) != int and type(a) != float:
         raise ValueError("a must be an integer")
-    elif type(b) != int or type(b) != float:
+    elif type(b) != int and type(b) != float:
         raise ValueError("b must be an integer")
     else:
         a = int(a)
