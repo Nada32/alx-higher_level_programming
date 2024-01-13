845b6fe3390cda0571aa45b3dd881e950be51230
Nada32 <nada0abou0alsoud@gmail.com>
2024-01-13 09:10:10 -0800

    d

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     for i in range(len(my_list) - 1):
         if idx > len(my_list) - 1:
             return (my_list)
-        elif idx <= len(my_list) - 1 and idx > 0:
+        elif idx <= len(my_list) - 1 and idx >= 0:
             my_list[idx] = element
         elif idx < 0:
             return (my_list)
