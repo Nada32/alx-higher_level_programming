96e7fbe0fa4026678b1df381594b85725a1f03f4
Nada32 <nada0abou0alsoud@gmail.com>
2024-01-20 10:37:36 -0800

    d

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(length(i)):
+            if j < length(i) - 1:
+                print("{} ".format(i[j]), end="")
+            elif j == length(i): - 1
+                print("{}$".format(i[j]))
+
