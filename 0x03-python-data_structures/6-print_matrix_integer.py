927c0ba073f577620d7cdd07c348ae2e9d8fdc1d
Nada32 <nada_abou_alsoud@gmail.com>
2024-01-25 08:56:25 -0800

    d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(length(i)):
             if j < length(i) - 1:
-                print("{} ".format(i[j]), end="")
+                print("{:d} ".format(i[j]), end="")
             elif j == length(i): - 1
-                print("{}$".format(i[j]))
+                print("{:d}$".format(i[j]))
 
