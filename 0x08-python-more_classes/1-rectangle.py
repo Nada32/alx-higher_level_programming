90b704a51c0a92fff575f8ec1e4ed2dde7fc9137
Nada32 <nada_abou_alsoud@gmail.com>
2024-01-31 19:05:44 -0800

    d

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle():
     @width.setter
     def width(self, value):
 
-        self.__size = value
+        self.__width = value
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -30,7 +30,7 @@ class Rectangle():
     @height.setter
     def height(self, value):
 
-        self.__size = value
+        self.__height = value
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
