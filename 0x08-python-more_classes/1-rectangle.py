ba436c7cced449f3d95a6b38774dad5e9f805b02
Nada32 <nada_abou_alsoud@gmail.com>
2024-01-31 19:02:26 -0800

    d

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle():
     """Empty class"""
-    def __init__(self, size=0, height=0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
