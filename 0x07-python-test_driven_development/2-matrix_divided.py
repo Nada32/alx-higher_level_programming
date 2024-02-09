a4bdf24798c1b496b5ae111df36e65f5c79a66cc
Nada32 <nada_abou_alsoud@gmail.com>
2024-02-09 14:33:25 -0800

    d

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,11 +4,20 @@ def matrix_divided(matrix, div):
     y1 = 0
     m = []
     mm = []
+    if type(div) != int and type(div) != float:
+        raise ValueError("a must be an integer")
     for i in matrix:
+        if type(i) != int and type(i) != float:
+                raise ValueError("a must be an integer")
         for j in i:
+            if type(j) != int and type(j) != float:
+                raise ValueError("a must be an integer")
             y0 = j / div
             y1 = round(y0, 2)
             mm.append(y1)
         m.append(mm)
         mm = []
     return m
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
