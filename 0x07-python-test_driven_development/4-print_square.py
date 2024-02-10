892940035d0aaaa8592b55c15c1bfdab988fc224
Nada32 <nada_abou_alsoud@gmail.com>
2024-02-10 02:33:08 -0800

    d

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""func module"""
+
+
+
 def print_square(size):
+    """func"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
             print("#", end="")
         print("")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
