be920c1833c846fd47db2f57500112b40bbb87b5
Nada32 <nada_abou_alsoud@gmail.com>
2024-02-10 03:50:40 -0800

    d

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""func module"""
+
+
+
 def text_indentation(text):
-    def text_indentation(text):
+    """func"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
     for i in text:
         if i == '.' or i == '?' or i== ':':
             print(i, end="")
@@ -8,3 +14,7 @@ def text_indentation(text):
         else:
             print(i, end="")
     print("")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
