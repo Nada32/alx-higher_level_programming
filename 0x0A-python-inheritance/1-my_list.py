7003aaaf40ced247b2c1b5b5d855a609add19cbb
Nada32 <nada_abou_alsoud@gmail.com>
2024-02-21 07:02:03 -0800

    d

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,11 +5,10 @@ contains the MyList class
 
 
 class Mylist(list):
-    """Class inherits from list"""
+    """Empty class"""
     pass
 
     @property
     def print_sorted(self):
         """meth"""
-
         print(sorted(list(self)))
