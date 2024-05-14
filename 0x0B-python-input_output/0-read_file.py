c0e54cdc5800719fb385485c2de51e52ffe0938a
Nada32 <nada0abou0alsoud@gmail.com>
2024-05-14 12:28:09 -0700

    d

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" module """
+
+
+
 def read_file(filename=""):
+    """ method """
     with open('filename', 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
