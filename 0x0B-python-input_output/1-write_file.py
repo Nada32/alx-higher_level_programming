83e593bd835f8d14e8c5e0cd46820a180ae558f3
Nada32 <nada0abou0alsoud@gmail.com>
2024-05-15 00:24:05 -0700

    d

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,8 @@
 
 def write_file(filename="", text=""):
     """ method """
+    z = 0
     with open(filename, 'w', encoding="utf-8") as f:
-        return len(f.write(text))
+        for line in f.write(text):
+            z = z + 1
+        return (z)
