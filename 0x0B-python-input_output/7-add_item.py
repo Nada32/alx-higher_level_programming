4e58d81f6a457c3e2589fad80b87da8459b254a3
Nada32 <nada0abou0alsoud@gmail.com>
2024-05-16 06:40:08 -0700

    D

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module"""
+
+
+import json
+import sys
+
+
+if __name__ == "__main__":
+    from 5-save_to_json_file import save_to_json_file
+    from 6-load_from_json_file.py import load_from_json_file
+    from sys import argv
+
+
+    filename = "add_item.json"
+    jsonlist = []
+
+    if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
+
+    for index in argv[1:]:
+        json_list.append(index)
+
+    save_to_json_file(json_list, filename)
