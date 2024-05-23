8dfe747d08207802cef5f32a1e60f064e4d42223
Nada32 <nada0abou0alsoud@gmail.com>
2024-05-23 04:22:38 -0700

    d

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT PRIVILEGE ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
-GRANT SELECT TO 'sammy'@'localhost' WITH GRANT OPTION;
+GRANT SELECT TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
