6cd92241629e4149b54036c46017e63ad022cd44
Nada32 <nada0abou0alsoud@gmail.com>
2024-05-23 04:28:18 -0700

    d

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- sql.
-CREATE DATABASE hbtn_0d_2;
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT PRIVILEGE ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
 GRANT SELECT TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
