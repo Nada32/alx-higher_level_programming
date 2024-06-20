be57c3682cca33d28380eba75d652e72796d240c
Nada32 <nada0abou0alsoud@gmail.com>
2024-06-20 07:10:29 -0700

    d

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// hi
+
+const x = process.argv;
+if (x.length === 2) {
+  console.log('No argument');
+} else if (x.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
