eab42e3f4b7728d017dabad48ca2d821102d32d0
Nada32 <nada0abou0alsoud@gmail.com>
2024-06-20 01:57:08 -0700

    d

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // hi
 
-const myvar = "JavaScript is amazing";
-const myvar2 = "Python is cool";
-const myvar3 = "C is fun";
+const myvar = 'JavaScript is amazing';
+const myvar2 = 'Python is cool';
+const myvar3 = 'C is fun';
 console.log(myvar3 + '\n' + myvar2 + '\n' + myvar);
