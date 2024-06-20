83fd3e9d30d91f6c80d5fe0a547dccdd31df4d7c
Nada32 <nada0abou0alsoud@gmail.com>
2024-06-19 22:35:27 -0700

    d

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//hi
+// hi
 
 const myVar = "JavaScript is amazing";
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-//hi
+// hi
 
 const myvar = "JavaScript is amazing";
 const myvar2 = "Python is cool";
 const myvar3 = "C is fun";
-console.log(myvar3);
-console.log(myvar2);
-console.log(myvar);
+console.log(myvar3 + '\n' + myvar2 + '\n' + myvar);
