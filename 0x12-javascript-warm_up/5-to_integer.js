7b6d5aeab15ada789810ea6b8a2fd91cbd22c022
Nada32 <nada0abou0alsoud@gmail.com>
2024-06-22 04:30:12 -0700

    D

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // hi
 
 const x = process.argv;
-if (typeof(Number(x[2])) != 'number') {
+if (typeof (Number(x[2])) !== 'number') {
   console.log('Not a number');
 } else {
   console.log('My number: ' + (x[2]));
