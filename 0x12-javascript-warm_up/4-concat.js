377013dba83df697589cd742ab8ce0a2716ee98c
Nada32 <nada0abou0alsoud@gmail.com>
2024-06-23 13:23:44 -0700

    d

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,5 +3,8 @@
 
 const x = process.argv;
 for (let i = 2; i < 4; i++) {
-  console.log(x[i]);
+  if ((x[i] === undefined) || (x.length === 3)) {
+    console.log(x[i] + ' is undefined');
+    break;
+  } console.log(x[i]);
 }
