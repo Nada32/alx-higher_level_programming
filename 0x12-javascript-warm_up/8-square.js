5afe77ff27b2cda4327f6ca9dbaac316a2eb1b26
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-10 06:39:23 -0700

    d

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// hi
+
+const x = process.argv;
+let i = 0;
+if (x[2] === undefined) {
+  console.log('Missing size');
+}
+for (; i < Number(x[2]); i++) {
+  let l = "";
+  for (let j = 0; j < Number(x[2]); j++) {
+    l += '*';
+  }
+  console.log(l);
+}
