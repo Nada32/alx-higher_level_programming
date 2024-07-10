1868c1111f850a550b46cba7e05de3a3229819c9
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-10 06:56:12 -0700

    d

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@
 
 const x = process.argv;
 let i = 0;
-if (x[2] === undefined) {
+if (typeof (Number(x[2])) !==  'number') {
   console.log('Missing size');
 }
 for (; i < Number(x[2]); i++) {
-  let l = "";
+  let l = '';
   for (let j = 0; j < Number(x[2]); j++) {
     l += 'X';
   }
