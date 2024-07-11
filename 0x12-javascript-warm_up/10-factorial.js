7e6cffe884827fd4167b4b2df4041d5f85a48274
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-10 23:33:37 -0700

    d

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 // hi
 
+function factoriall (a) {
+  let z = 1;
+  if ((a === 1) || (a === 0) || (isNaN(a))) {
+    return (1);
+  }
+  else {
+    for (let i = 1; i <= a; i++) {
+      z = z * i;
+    }
+    return (z);
+  }
+}
 const x = process.argv;
 const xx = Number(x[2]);
-let z = 1;
-if ((xx === 1) || (xx === 0) || (isNaN(x[2]))) {
-    console.log(1);
-  }
-else {
-  for (let i = 1; i <= xx; i++) {
-    z = z * i;
-  }
-  console.log(z);
-}
+console.log(factoriall(xx));
