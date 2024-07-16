749f1e977dda00774232305b330dac7627ba9167
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-16 11:29:11 -0700

    d

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// hi
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w !== undefined && h !== undefined) && (w > 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let l = '';
+      for (let j = 0; j < this.width; j++) {
+        l += 'X';
+      }
+      console.log(l);
+    }
+  }
+};
