2530c766a494ab799525066cd7f5012f921e298b
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-27 09:43:30 -0700

    d

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+// hi
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    x = MySQLdb.connect (
+        host = 3306,
+        user = arg[1],
+        pw = arg[2],
+        database = arg[3]
+        )
+    cursorObject = dataBase.cursor()
+
+    query = "SELECT STATES FROM STUDENT ORDERED BY ID"
+    cursorObject.execute(query)
+
+    myresult = cursorObject.fetchall()
+
+    for x in myresult:
+        print(x)
+
+    dataBase.close()
