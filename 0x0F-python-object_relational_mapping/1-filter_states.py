b88b523bc94b605f0c106e340a07a97c8b102ef2
Nada32 <nada0abou0alsoud@gmail.com>
2024-07-28 06:30:17 -0700

    d

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+hi
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    hi
+    """
+    db = MySQLdb.connect (
+        host = "localhost",
+        user = sys.argv[1],
+        password = sys.argv[2],
+        db = sys.argv[3],
+        port = 3306
+        )
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states WHERE NAME LIKE 'H' ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
